fb36460a8f31d4e65906e6729ae9f5a7b104bea5
kilep10 <you@example.com>
2024-09-23 00:25:39 +0500

    php

---
+++ resources/views/home.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <div class="header-logo">
+                Лого
+            </div>
+            <div class="header-nav">
+                <div class="header-nav-item">
+                    <a href="#">Главная</a>
+                </div>
+                <div class="header-nav-item">
+                    <a href="#">Массивы</a>
+                </div>
+            </div>
+        </div>
+        <div class="main">
+            <div class="main-text">
+                тексКОнтентоной части/text contentnoi chasti
+            </div>
+            <div class="main-img">
+                <img src="" alt="">
+            </div>
+        </div>
+        <div class="footer">
+            <div class="footer-kop">
+                ©Копирайт
+            </div>
+            <div class="footer-fio">
+                Лбедев Дмитрий Константинович
+            </div>
+            <div class="footer-date">
+                2024.
+            </div>
+        </div>
+    </div>
+</body>
+</html>
